23dac6429d3745e76b701c63c4e0b6a878be6754
leeJhonatan <lee_jhonatan_22@hotmail.com>
2024-10-22 22:55:07 -0500

    imp(home): Customized HomeScreen - Changed icons - Added toolbar

--- composeApp/src/commonMain/kotlin/com/edlabcode/rickmortyapp/ui/core/navigation/bootomnavigation/BottomBarItem.kt
+++ composeApp/src/commonMain/kotlin/com/edlabcode/rickmortyapp/ui/core/navigation/bootomnavigation/BottomBarItem.kt
@@ -1,11 +1,16 @@
 package com.edlabcode.rickmortyapp.ui.core.navigation.bootomnavigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
+
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.edlabcode.rickmortyapp.ui.core.navigation.Routes
+import org.jetbrains.compose.resources.painterResource
+import rickmortyapp.composeapp.generated.resources.Res
+import rickmortyapp.composeapp.generated.resources.characters
+import rickmortyapp.composeapp.generated.resources.player
 
 sealed class BottomBarItem {
     abstract val route: String
@@ -16,7 +21,10 @@ sealed class BottomBarItem {
         override val route: String = Routes.Episodes.route,
         override val title: String = "Episodes",
         override val icon: @Composable () -> Unit = {
-            Icon(imageVector = Icons.Default.Home, contentDescription = "")
+            Icon(
+                painter = painterResource(Res.drawable.player), contentDescription = "",
+                modifier = Modifier.size(24.dp)
+            )
         }
     ) : BottomBarItem()
 
@@ -24,7 +32,10 @@ sealed class BottomBarItem {
         override val route: String = Routes.Characters.route,
         override val title: String = "Characters",
         override val icon: @Composable () -> Unit = {
-            Icon(imageVector = Icons.Default.Person, contentDescription = "")
+            Icon(
+                painter = painterResource(Res.drawable.characters), contentDescription = "",
+                modifier = Modifier.size(24.dp)
+            )
         }
     ) : BottomBarItem()
 
--- composeApp/src/commonMain/kotlin/com/edlabcode/rickmortyapp/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/edlabcode/rickmortyapp/ui/home/HomeScreen.kt
@@ -1,22 +1,37 @@
 package com.edlabcode.rickmortyapp.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.edlabcode.rickmortyapp.ui.core.BackgroundPrimaryColor
+import com.edlabcode.rickmortyapp.ui.core.BackgroundSecondaryColor
+import com.edlabcode.rickmortyapp.ui.core.BackgroundTertiaryColor
+import com.edlabcode.rickmortyapp.ui.core.DefaultTextColor
+import com.edlabcode.rickmortyapp.ui.core.Green
 import com.edlabcode.rickmortyapp.ui.core.navigation.bootomnavigation.BottomBarItem
 import com.edlabcode.rickmortyapp.ui.core.navigation.bootomnavigation.BottomBarItem.Characters
 import com.edlabcode.rickmortyapp.ui.core.navigation.bootomnavigation.BottomBarItem.Episodes
 import com.edlabcode.rickmortyapp.ui.core.navigation.bootomnavigation.NavigationBottomWrapper
+import org.jetbrains.compose.resources.painterResource
+import rickmortyapp.composeapp.generated.resources.Res
+import rickmortyapp.composeapp.generated.resources.ricktoolbar
 
 @Composable
 fun HomeScreen(mainNavController: NavHostController) {
@@ -24,7 +39,8 @@ fun HomeScreen(mainNavController: NavHostController) {
     val navController = rememberNavController()
 
     Scaffold(
-        bottomBar = { BottomNavigation(items, navController) }
+        bottomBar = { BottomNavigation(items, navController) },
+        topBar = { TopBar() }
     ) { padding ->
         Box(modifier = Modifier.padding(padding)) {
             NavigationBottomWrapper(
@@ -37,13 +53,37 @@ fun HomeScreen(mainNavController: NavHostController) {
 }
 
 @Composable
+fun TopBar() {
+    Box(
+        modifier = Modifier.fillMaxWidth().background(BackgroundPrimaryColor),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Image(
+            painter = painterResource(Res.drawable.ricktoolbar),
+            contentDescription = null,
+            modifier = Modifier.height(100.dp)
+                .padding(start = 16.dp, top = 10.dp, bottom = 10.dp)
+        )
+    }
+
+}
+
+@Composable
 fun BottomNavigation(items: List<BottomBarItem>, navController: NavHostController) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
 
-    NavigationBar {
+    NavigationBar(
+        containerColor = BackgroundSecondaryColor,
+        contentColor = Green
+    ) {
         items.forEach { item ->
             NavigationBarItem(
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = Green,
+                    selectedIconColor = BackgroundTertiaryColor,
+                    unselectedIconColor = Green
+                ),
                 icon = item.icon,
                 onClick = {
                     navController.navigate(item.route) {
@@ -57,7 +97,7 @@ fun BottomNavigation(items: List<BottomBarItem>, navController: NavHostControlle
                     }
                 },
                 selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
-                label = { Text(item.title) },
+                label = { Text(item.title, color = DefaultTextColor) },
                 alwaysShowLabel = false
             )
         }
